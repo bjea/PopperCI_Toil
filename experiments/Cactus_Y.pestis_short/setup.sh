2028f835abd96317d3c9eb2c1e73b06041f629f7
Bettie Jea <bjea@Betties-MacBook-Pro.local>
2017-12-08 16:09:37 -0800

    Edited the filepath corresponding to renamed folders.

--- experiments/Cactus_Y.pestis_short/setup.sh
+++ experiments/Cactus_Y.pestis_short/setup.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
 # Use this setup.sh to launch a leader node (`toil launch-cluster`) on AWS and upload required files to such leader
-# node (`toil rsync-cluster`). Before running this shell script (`bash setup.sh`), a user needs to copy the ~/.aws and
-# ~/.ssh (or create a new ssh key) to the current working directory.
+# node (`toil rsync-cluster`). Before running this shell script (`bash setup.sh`), a user needs to copy the .aws/ and
+# .ssh/ (or create a new ssh key) folders to the home directory (~/) if they are not already there.
 
-docker run --rm -v `pwd`:/root --workdir=/root -p 5050:5050 quay.io/ucsc_cgl/toil:3.11.0 toil launch-cluster \
+docker run --rm -v $HOME:/root -v `pwd`:/curr --workdir=/curr -p 5050:5050 quay.io/ucsc_cgl/toil:3.11.0 toil launch-cluster \
     cactuscluster --leaderNodeType t2.medium --keyPairName bjea-US-WEST-2 --zone us-west-2c
 
-docker run --rm -v `pwd`:/root --workdir=/root -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
-    toil rsync-cluster --insecure cactuscluster /root/experiments/pestis/datapackages/pestis-short-aws-seqFile.txt :/tmp
+docker run --rm -v $HOME:/root -v `pwd`:/curr --workdir=/curr -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
+    toil rsync-cluster --insecure cactuscluster /curr/experiments/Cactus_Y.pestis_short/datapackages/pestis-short-aws-seqFile.txt :/tmp
 
-docker run --rm -v `pwd`:/root --workdir=/root -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
-    toil rsync-cluster --insecure cactuscluster /root/experiments/pestis/datapackages/GCF_000169655.1_ASM16965v1_genomic.fna :/tmp
+docker run --rm -v $HOME:/root -v `pwd`:/curr --workdir=/curr -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
+    toil rsync-cluster --insecure cactuscluster /curr/experiments/Cactus_Y.pestis_short/datapackages/GCF_000169655.1_ASM16965v1_genomic.fna :/tmp
 
-docker run --rm -v `pwd`:/root --workdir=/root -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
-    toil rsync-cluster --insecure cactuscluster /root/experiments/pestis/datapackages/GCF_000006645.1_ASM664v1_genomic.fna :/tmp
+docker run --rm -v $HOME:/root -v `pwd`:/curr --workdir=/curr -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
+    toil rsync-cluster --insecure cactuscluster /curr/experiments/Cactus_Y.pestis_short/datapackages/GCF_000006645.1_ASM664v1_genomic.fna :/tmp
 
-docker run --rm -v `pwd`:/root --workdir=/root -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
-    toil rsync-cluster --insecure cactuscluster /root/experiments/pestis/datapackages/GCF_000182485.1_ASM18248v1_genomic.fna :/tmp
+docker run --rm -v $HOME:/root -v `pwd`:/curr --workdir=/curr -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
+    toil rsync-cluster --insecure cactuscluster /curr/experiments/Cactus_Y.pestis_short/datapackages/GCF_000182485.1_ASM18248v1_genomic.fna :/tmp
 
-docker run --rm -v `pwd`:/root --workdir=/root -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
-    toil rsync-cluster --insecure cactuscluster /root/experiments/pestis/datapackages/GCF_000013805.1_ASM1380v1_genomic.fna :/tmp
+docker run --rm -v $HOME:/root -v `pwd`:/curr --workdir=/curr -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
+    toil rsync-cluster --insecure cactuscluster /curr/experiments/Cactus_Y.pestis_short/datapackages/GCF_000013805.1_ASM1380v1_genomic.fna :/tmp
 
-#docker run --rm -v `pwd`:/root --workdir=/root -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
-#    toil rsync-cluster --insecure cactuscluster /root/experiments/pestis/setup_leaderNode.sh :/tmp
+docker run --rm -v $HOME:/root -v `pwd`:/curr --workdir=/curr -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
+    toil rsync-cluster --insecure cactuscluster /curr/experiments/Cactus_Y.pestis_short/leaderNode.sh :/tmp
 
-docker run --rm -v `pwd`:/root --workdir=/root -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
-    toil rsync-cluster --insecure cactuscluster /root/experiments/pestis/run.sh :/tmp
+docker run --rm -v $HOME:/root -v `pwd`:/curr --workdir=/curr -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
+    toil rsync-cluster --insecure cactuscluster /curr/experiments/Cactus_Y.pestis_short/datapackages/blockTrim1.xml :/tmp
 
-docker run --rm -v `pwd`:/root --workdir=/root -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
-    toil rsync-cluster --insecure cactuscluster /root/experiments/pestis/datapackages/blockTrim1.xml :/tmp
+docker run --rm -v $HOME:/root -v `pwd`:/curr --workdir=/curr -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
+    toil rsync-cluster --insecure cactuscluster /curr/experiments/Cactus_Y.pestis_short/datapackages/blockTrim3.xml :/tmp
 
-docker run --rm -v `pwd`:/root --workdir=/root -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" quay.io/ucsc_cgl/toil:3.11.0 \
-    toil rsync-cluster --insecure cactuscluster /root/experiments/pestis/datapackages/blockTrim3.xml :/tmp
 
-docker run --rm -it -v `pwd`:/root --workdir=/root -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" \
-    quay.io/ucsc_cgl/toil:3.11.0 toil ssh-cluster cactuscluster bash /tmp/run.sh
 
 exit 0
--- experiments/Cactus_Y.pestis_short/validate.sh
+++ experiments/Cactus_Y.pestis_short/validate.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e TOIL_AWS_ZONE=us-west-2c
 
 blockTrim=(1 3)
@@ -8,9 +8,9 @@ for i in "${blockTrim[@]}"
 do
     for j in "${minNodes[@]}"
     do
-        docker run --rm -v `pwd`:/root --workdir=/root -p 550:550 -e "TOIL_AWS_ZONE=us-west-2c" \
-        quay.io/ucsc_cgl/toil:3.11.0 toil rsync-cluster --insecure cactuscluster :/tmp/pestis_output_new_$i-$j.hal \
-        /root/experiments/pestis/
+        docker run --rm -v $HOME:/root -v `pwd`:/curr --workdir=/curr -p 550:550 -e "TOIL_AWS_ZONE=us-west-2c" \
+        quay.io/ucsc_cgl/toil:3.11.0 toil rsync-cluster --insecure cactuscluster :/tmp/pestis_output_$i-$j.hal \
+        /curr/experiments/Cactus_Y.pestis_short/
 
     done;
 done;
@@ -21,10 +21,11 @@ do
     for j in "${minNodes[@]}"
     do
         docker run --rm -ti -v `pwd`:/root --entrypoint=h5diff hdfgroup/hdf5-json \
-        /root/experiments/pestis/pestis_output$i.hal /root/experiments/pestis/pestis_output_new_$i-$j.hal
+        /root/experiments/Cactus_Y.pestis_short/pestis_output$i.hal \
+        /root/experiments/Cactus_Y.pestis_short/pestis_output_$i-$j.hal
 
     done;
 done;
 
 
-exit
+exit 0
