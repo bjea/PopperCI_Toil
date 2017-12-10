b0b300456009638236567d96c5196bb64e36ec58
Bettie Jea <bjea@Betties-MacBook-Pro.local>
2017-12-09 18:32:32 -0800

    Added back a required bind mount $HOME that had been deleted by accident.

--- experiments/Cactus_Y.pestis_short/run.sh
+++ experiments/Cactus_Y.pestis_short/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -it -v `pwd`:/curr --workdir=/curr -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" \
+docker run --rm -it -v $HOME:/root -v `pwd`:/curr --workdir=/curr -p 5050:5050 -e "TOIL_AWS_ZONE=us-west-2c" \
     quay.io/ucsc_cgl/toil:3.11.0 toil ssh-cluster cactuscluster bash /tmp/leaderNode.sh
 
 exit 0
